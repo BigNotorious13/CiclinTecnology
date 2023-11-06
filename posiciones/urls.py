b78d2131e7c96e0cc8e8f1fdb97f5c016f7f259d
Notorious <luisg13011@hotmail.com>
2023-11-05 23:16:27 -0600

    vistas enlazadas 2

--- posiciones/urls.py
+++ posiciones/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('login/', views.login_juez, name='login'),
     path('inicio/', views.inicio, name='inicio'),
     path('horarios/', views.horario, name='horarios'),
+    path('posiciones/', views.posiciones, name='posiciones'),
+    path('resultados/', views.resultados, name='resultados'),
+    path('categorias/elite/', views.categoria_elite, name='cate_elite'),
+    path('categorias/femenil/', views.categoria_femenil, name='cate_femenil'),
+    path('categorias/master30', views.categoria_master30, name='cate_master30'),
+    path('categorias/master40', views.categoria_master40, name='cate_master40'),
 ]
--- posiciones/views.py
+++ posiciones/views.py
@@ -6,11 +6,36 @@ def principal(request):
     return render(request, 'principal.html')
 
 
+def posiciones(request):
+    return render(request, 'posiciones/posicion.html')
+
+def resultados(request):
+    return render(request, 'resultados/resultado.html')
+
+
 def login_juez(request):
     return render(request, 'iniJuez.html')
 
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+
 def horario(request):
     return render(request, 'horarios/horario.html')
+
+
+def categoria_elite(request):
+    return render(request, 'categoria/elite.html')
+
+
+def categoria_femenil(request):
+    return render(request, 'categoria/femenil.html')
+
+
+def categoria_master30(request):
+    return render(request, 'categoria/master30.html')
+
+
+def categoria_master40(request):
+    return render(request, 'categoria/master40.html')
