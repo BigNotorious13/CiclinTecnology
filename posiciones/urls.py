d8fca911c02bc7b8d48e417cd510e2dc14cd003f
Notorious <luisg13011@hotmail.com>
2023-11-05 22:15:21 -0600

    vistas enlazadas

--- posiciones/urls.py
+++ posiciones/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='home'),
+    path('', views.principal, name='home'),
+    path('login/', views.login_juez, name='login'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('horarios/', views.horario, name='horarios'),
 ]
--- posiciones/views.py
+++ posiciones/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def inicio(request):
+def principal(request):
     return render(request, 'principal.html')
+
+
+def login_juez(request):
+    return render(request, 'iniJuez.html')
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
+def horario(request):
+    return render(request, 'horarios/horario.html')
